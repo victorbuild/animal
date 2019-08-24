993709820881ec3fa63179fef0d1b0002d7612ad
victor <alisw551@gmail.com>
2019-08-24 16:47:11 +0800

    修改動物功能

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -71,7 +71,8 @@ class AnimalController extends Controller
      */
     public function update(Request $request, Animal $animal)
     {
-        //
+        $animal->update($request->all());
+        return response($animal, Response::HTTP_OK);
     }
 
     /**
